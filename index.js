f0f1925756ec4c2b4e0729e539b86c7c30351432
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-05-01 00:35:47 +0600

    update implementation

--- index.js
+++ index.js
@@ -50,7 +50,10 @@ async function run(){
         });
 
         //PUT
-        
+        app.put('/product/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:ObjectId(id)};
+        })
 
 
 
