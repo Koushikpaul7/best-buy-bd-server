d5d13ba9af11fdac2e1de8770a0c5718bb351a33
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-04-30 11:18:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors= require('cors');
+const app=express();
+const port= process.env.PORT||5000;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = "mongodb+srv://dbUser4:<password>@cluster0.6r7dg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log('running my best buy');
+  client.close();
+});
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Running server')
+});
+
+app.listen(port,()=>{
+    console.log('My server is running');
+})
