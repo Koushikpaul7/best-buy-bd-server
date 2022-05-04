f6b2c62b874df7b6661fd343cecd4d96b64ea3cb
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-05-05 01:30:36 +0600

    restock updated

--- index.js
+++ index.js
@@ -65,6 +65,21 @@ async function run(){
         });
 
 
+        app.put('/product/:id',async(req,res)=>{
+            const id=req.params.id;
+            const updatedProduct=req.body;
+            const filter= {_id:ObjectId(id)};
+            const options={upsert:true};
+            const updatedDoc={
+                $set:{
+                    quantity:updatedProduct.quantity
+                }
+            };
+            const result=await productCollection.updateOne(filter,updatedDoc,options);
+            res.send(result);
+        });
+
+
 
     }
     finally {
